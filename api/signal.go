1f15934474c5c9aa9edc27d917a90ba1c848d85d
yayu <tim.yayu.cheng@gmail.com>
2023-02-03 15:12:41 +0800

    Annotation: 信号处理器，平滑关闭，平滑重启

--- api/signal.go
+++ api/signal.go
@@ -16,30 +16,45 @@ const (
 
 func signalHandler(llist ...net.Listener) {
 	ch := make(chan os.Signal, 10)
-	signal.Notify(ch, syscall.SIGINT, syscall.SIGUSR2)
+	signal.Notify(ch, syscall.SIGINT, syscall.SIGUSR2) // bug: 没有注册 syscall.SIGTERM
 	for {
 		sig := <-ch
 		switch sig {
 		case syscall.SIGINT, syscall.SIGTERM:
+
+			// 令 ch 停止接收信号
 			signal.Stop(ch)
 			util.Log.Error("receive signal TERM/INT stop the process")
+
+			// 关闭所有 listener
 			for _, lis := range llist {
 				lis.Close()
 			}
+
+			// 全局 context cancel
 			util.CancelFunc()
+
+			// 等待一段时间
 			<-time.After(GraceStopTimeout)
 			util.Log.Error("signal handler done now end the main process")
 			return
 		case syscall.SIGUSR2:
+
 			util.Log.Error("receive signal USR2 restart process")
+
+			// 启动一个新进程
 			count, err := util.GraceNet.StartProcess()
 			if err != nil {
 				util.Log.Error("restart process fail", err)
 				return
 			}
+
+			// 关闭原来的 listener
 			for _, lis := range llist {
 				lis.Close()
 			}
+
+			// 全局 context cancel
 			util.CancelFunc()
 
 			util.Log.Error("signal handler done now end the main process", count, err)
